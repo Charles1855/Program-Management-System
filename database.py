49ba1d4fa739ec4c99fc163d17f16b2cbb010c7c
Charles Chima <charliediego22@gmail.com>
2026-01-14 17:17:19 +0000

    Created tables for the database

--- database.py
+++ database.py
@@ -2,3 +2,67 @@ import sqlite3
 
 def connect_db():
     return sqlite3.connect("mhub.db")
+
+def create_tables():
+    conn = connect_db()
+    cursor = conn.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        role TEXT NOT NULL,
+        created_at TEXT NOT NULL
+    )
+    """)
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS participants (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        full_name TEXT NOT NULL,
+        gender TEXT,
+        phone TEXT,
+        email TEXT,
+        location TEXT,
+        date_registered TEXT NOT NULL
+    )
+    """)
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS programs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        program_name TEXT NOT NULL,
+        description TEXT,
+        start_date TEXT,
+        end_date TEXT,
+        facilitator TEXT,
+        created_at TEXT NOT NULL
+    )
+    """)
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS enrollments (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        participant_id INTEGER,
+        program_id INTEGER,
+        enroll_date TEXT NOT NULL,
+        FOREIGN KEY (participant_id) REFERENCES participants(id),
+        FOREIGN KEY (program_id) REFERENCES programs(id)
+    )
+    """)
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS attendance (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        participant_id INTEGER,
+        program_id INTEGER,
+        date TEXT NOT NULL,
+        status TEXT NOT NULL,
+        FOREIGN KEY (participant_id) REFERENCES participants(id),
+        FOREIGN KEY (program_id) REFERENCES programs(id)
+    )
+    """)
+
+conn.commit()
+conn.close()
